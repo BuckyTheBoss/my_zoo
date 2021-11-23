37245769e3c203e5bf94d813990e272a7507c668
BuckytheBoss <buchman.avi@gmail.com>
2021-11-23 11:36:38 +0200

    added model

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=200)
+    legs = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+    speed = models.PositiveIntegerField()
+
